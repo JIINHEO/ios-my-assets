d17e9b5f2846d9003fe2888dc7c37dd810affd9a
JIINHEO <gjwldls123@gmail.com>
2021-12-07 18:36:25 +0900

    first commit

---
+++ ios-my-assets/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios-my-assets
+//
+//  Created by 허지인 on 2021/12/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios-my-assets/ios_my_assetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_my_assetsApp.swift
+//  ios-my-assets
+//
+//  Created by 허지인 on 2021/12/07.
+//
+
+import SwiftUI
+
+@main
+struct ios_my_assetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
